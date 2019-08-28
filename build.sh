f7a000f0b05c6466b0768a43f4b89dc8a0e7f2fc
腹黒い茶 <haraguroicha@tipsy.coffee>
2019-08-28 18:18:35 +0800

    add rev hash

--- build.sh
+++ build.sh
@@ -7,6 +7,17 @@ mkdir -p "${TARGET}"
 
 cd "${DIR}/src"
 
+isDirty="`git diff --quiet || echo '-dirty'`"
+shortVer="`git rev-parse --short master`${isDirty}"
+
+if [ "${isDirty}" != "" ]; then
+    rm "${DIR}/src/data/rev.yaml"
+fi
+
+if [ ! -f "${DIR}/src/data/rev.yaml" ]; then
+    echo "AbbreviatedHash: ${shortVer}" > "${DIR}/src/data/rev.yaml"
+fi
+
 HUGO_ENV=production hugo -b "/"
 
 cd "${DIR}"
